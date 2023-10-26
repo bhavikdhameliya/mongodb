0810c30554289cfd33df24a70944ade5b7bedf45
vivek prajapati <vivekprajapati2536@gmail.com>
2023-10-26 18:00:43 +0530

    js

--- lookup.js
+++ lookup.js
@@ -1,15 +1,79 @@
-db.students.aggregate([
-        {
-            $lookup: {
-                   from: "teachers",
-                   localField: "faculty",
-                   foreignField: "tname",
-                   as: "faculty"
-                 }
-        },
-        {
-            $project: {
-                "faculty._id": 0,
-            }
-        }
-        ])
+// db.students.aggregate([
+//         {
+//             $lookup: {
+//                    from: "teachers",
+//                    localField: "faculty",
+//                    foreignField: "tname",
+//                    as: "faculty"
+//                  }
+//         },
+//         {
+//             $project: {
+//                 "faculty._id": 0,
+//             }
+//         }
+//         ])
+
+
+db.student.aggregate([
+    {
+        $lookup: {
+               from: "teacher",
+               localField: "faculty",
+               foreignField: "tname",
+               as: "details"
+             }
+    },
+    {
+        $unwind: "$details"
+    }
+    ])
+
+//1
+   //ex
+
+   db.student.aggregate([
+    {
+        $lookup: {
+                from: "teacher",
+                localField: "faculty",
+                foreignField: "tname",
+                as: "facultys"
+             }
+    },
+    {
+    $unwind: "$facultys"
+},
+    
+      {
+  $group: {
+     _id: null,
+     totalQuantity: { $avg: "$fees" },
+  sumsalary :{ $avg: "$facultys.salary"},
+  totalQuantity: { $sum: "$fees" },
+  sumsalary :{ $sum: "$facultys.salary"},
+  totalQuantity: { $count:{}},
+  totalQuantity: { $min:"$fees"},
+    sumsalary :{ $min: "$facultys.salary"},
+    totalQuantity: { $max:"$fees"},
+    sumsalary :{ $max: "$facultys.salary"},
+    totalQuantity:{$first:"$sname"},
+    totalQuantity:{$last:"$sname"}
+  }
+}
+    ])
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
